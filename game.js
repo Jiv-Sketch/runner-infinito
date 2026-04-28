7bc7f113a38096907ecd1903d5b821e6f755078a
Jiv-Sketch <souzakairo619@gmail.com>
2026-04-28 19:29:45 -0300

    Atualizar o game.js

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ function atualizar() {
 
   if (gp) {
     // botão A (padrão Xbox)
-    if (gp.buttons[97].pressed && !player.isJumping && gameRunning) {
+    if (gp.buttons[a].pressed && !player.isJumping && gameRunning) {
       player.velocityY = -15;
       player.isJumping = true;
     }
